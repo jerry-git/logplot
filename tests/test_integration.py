f19e322bb5c6a7900296c581d52e19655c0849fa
Jerry Pussinen <jerry.pussinen@gmail.com>
2018-10-27 15:54:31 +0200

    Add semi dummy integration test

---
+++ tests/test_integration.py
@@ -0,0 +1,24 @@
+from os import path as osp
+from unittest.mock import patch
+
+from click.testing import CliRunner
+import pytest
+
+from logplot.cli import log_plot
+
+DATA_DIR = osp.join(osp.dirname(__file__), "data", "integration")
+LOG = osp.join(DATA_DIR, "log.txt")
+CONF = osp.join(DATA_DIR, "user_conf.yaml")
+
+
+@pytest.fixture
+def cli_runner():
+    return CliRunner()
+
+
+class TestPlotCreation:
+    @patch("logplot.plot.plt.show")
+    def test_it_plots(self, show_plot, cli_runner):
+        res = cli_runner.invoke(log_plot, [LOG, "-c", CONF], catch_exceptions=False)
+        assert not res.exception
+        show_plot.assert_called_once()
